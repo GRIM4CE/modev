04306d9abf11755fd069b434f3ca6f1c3391f0a5
Jon Leibham <jleibham@Jons-MBP.attlocal.net>
2016-08-17 11:21:59 -0500

    edits

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,10 @@
   function closePopUp() {
     $('.pop-up-container').addClass('hidden');
   }
+
+  $(document).mouseup(function (e) {
+    var container = $('.pop-up-box');
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      $('.pop-up-container').addClass('hidden');
+    }
+  });

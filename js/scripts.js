d54b87fe92116d667f8febdbea74148bce17cc34
Jon Leibham <jleibham@Jons-MacBook-Pro.local>
2016-08-23 11:23:58 -0500

    changes for the Connected Health Sponsorship Page

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 'use strict';
 
   function popUp(element) {
-    $(element).siblings('.pop-up-container').removeClass('hidden');
+    $(element).parent().next('.pop-up-container').removeClass('hidden');
+    $(document).find('body').css('overflow', 'hidden');
   }
 
   function closePopUp() {
     $('.pop-up-container').addClass('hidden');
+    $(document).find('body').css('overflow', 'auto');
   }
 
   $(document).mouseup(function (e) {
     var container = $('.pop-up-box');
     if (!container.is(e.target) && container.has(e.target).length === 0) {
       $('.pop-up-container').addClass('hidden');
+      $(document).find('body').css('overflow', 'auto');
     }
   });

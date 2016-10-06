3e62f86f5d8abd4d49a3df29122beb4a5fe12496
Jon Leibham <jleibham@Jons-MacBook-Pro-2.local>
2016-10-06 10:29:39 -0500

    edits to schedule

---
+++ src/js/code-writer.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('.quote-image').hover(function(){
+    $(this).parents('.speaker-container').find('.hover-quote').fadeIn(300);
+   });
+
+   $('.quote-image').mouseout(function(){
+     $(this).parents('.speaker-container').find('.hover-quote').fadeOut(300);
+   });
+
+  $('.read-more-button').click(function() {
+    var button = this;
+    var parent = button.parentNode;
+    var text = $(parent).children('.read-more-text');
+
+    if (!(text).is(":visible")) {
+      text.css("display", "block");
+      button.innerHTML = "";
+      button.innerHTML = "Read Less";
+    } else {
+      text.css("display", "none");
+      button.innerHTML = "";
+      button.innerHTML = "Read More";
+    }
+  });
+});
